3fce1927345f2e9ee56767465b65f455b9f8d98b
kyle kenney <kairukenney@gmail.com>
2017-09-06 16:41:50 -0700

    create-react-app with some starter code

---
+++ src/App.js
@@ -0,0 +1,53 @@
+// Libs
+import React, { Component } from 'react';
+
+// Component
+export default class App extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      names: [
+        { name: 'Joel' },
+        { name: 'Alena' },
+        { name: 'Andrew' },
+        { name: 'Thera' }
+      ]
+    }
+  }
+  
+  handleChange(event) {
+    if(event.key === 'Enter') {
+      let newName = { name: event.target.value }
+      let newNames = this.state.names.concat(newName);
+      event.target.value = '';
+      this.setState({ names: newNames });
+    }
+  }
+  
+  handleRemove(i) {
+    var newNames = this.state.names;
+    newNames.splice(i, 1);
+    this.setState({ names: newNames });
+  }
+  
+  render() {    
+    let guests = this.state.names.map((name, i) => (
+      <li key={name.name}>
+        {name.name}
+        <button onClick={this.handleRemove.bind(this, i)}>Remove</button>
+      </li>
+    ));
+    
+    return (
+      <div className="guest-list">
+        <h1>Guest List</h1>
+        <input type="text" placeholder="Invite Someone" value={this.state.newName} onKeyDown={this.handleChange.bind(this)} />
+        <ul>
+          
+          {guests}
+        
+        </ul>  
+      </div>
+    );
+  }
+}
